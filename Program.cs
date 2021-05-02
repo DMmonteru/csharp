1e9b01a29e66a8f7dde55f96e9d8747eab027fc3
Denis Mutsevich <46190811+DMmonteru@users.noreply.github.com>
2021-05-02 23:58:16 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,104 +1,145 @@
 ﻿using System;
 
-namespace _1761_integrals
+namespace _1761
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Выберите метод:");
-            Console.WriteLine("1) Трапеций\n2) Симпсона\n");
+            Console.WriteLine("1) Половинного деления\n2) Итераций\n3) Ньютона\n");
             int choice = int.Parse(Console.ReadLine());
-            if (choice == 1) { Trapezoid(); }
-            else if (choice == 2) { Simpson(); }
+            if (choice == 1) { Console.WriteLine($"f(x) = 0 при x = {Half_division()}"); }
+            else if (choice == 2) { Console.WriteLine($"f(x) = 0 при x = {Iteration()}"); }
+            else if (choice == 3) { Console.WriteLine($"f(x) = 0 при x = {Newton()}"); }
             else { Console.WriteLine("Неверное значение. Перезапустите программу."); }
             Console.ReadKey();
         }
 
         // 1
-        private static void Trapezoid()
+        private static double Half_division()
         {
-            // #10
-            // [0; sqrt(3)]
-            // n = 48
-            // f(x) = x*arctgx
-            // F(x) = 0.5*x^2*arctgx - 0.5*x + 0.5*arctgx
-            Console.WriteLine("\nМетод трапеций:");
-            Console.WriteLine("f(x) = x*arctgx");
+            // #30
+            // [2; 3] -> 2.4200
+            // y = 0.6 * 3^x - 2.3 * x - 3
+            Console.WriteLine("\nМетод половинного деления:");
+            Console.WriteLine("f(x) = 0.6 * 3^x - 2.3 * x - 3");
 
-            const double a = 0, n = 48;
-            double b = Math.Sqrt(3);
-            double h = (b - a) / n;
-            // I ~= 0.5*h(y0 + yn + 2*sum(yi)) -> i from 1 to n-1
-            double y0 = Function(a), yn = Function(b), sum = 0;
-            double xi = a;
-            for (int i = 1; i < n; i++)
+            double x = 2.5, a = 2, b = 3; // (2 + 3) / 2
+            if (Func_hd(x) == 0)
             {
-                xi = xi + h;
-                sum = sum + Function(xi);
+                return Math.Round(x, 4);
             }
-            // По формуле Ньютона-Лейбница
-            double integral = Math.Round(Bigfunction(b) - Bigfunction(a), 5);
-            double s = Math.Round(0.5 * h * (y0 + yn + 2 * sum), 5);
-            double abs = Math.Round(s - integral, 5);
-            double otn = Math.Round(Math.Abs(abs) / integral, 4) * 100;
-            Console.WriteLine($"F(b) - F(a) = {integral}"); // действительное значение
-            Console.WriteLine($"I ~= {s}"); // эксперементальное значение
-            Console.WriteLine($"Абсолютная погрешность ~= {abs}");
-            Console.WriteLine($"Относительная точность приближенного значения интеграла ~= {otn}%");
+            else
+            {
+                int counter = 0;
+                do
+                {
+                    if (Func_hd(x) * Func_hd(a) < 0)
+                    {
+                        b = x;
+                    }
+                    else
+                    {
+                        a = x;
+                    }
+                    x = (a + b) / 2;
+                    counter++;
+                }
+                while (Math.Abs(b - a) > 1e-4);
+                Console.WriteLine($"Количество делений: {counter}");
+            }
+            return Math.Round(x, 4);
         }
-        // функция
-        private static double Function(double x)
+        private static double Func_hd(double x)
         {
-            return x * Math.Atan(x);
-        }
-        // первообразная
-        private static double Bigfunction(double x)
-        {
-            return 0.5 * x * x * Math.Atan(x) - 0.5 * x + 0.5 * Math.Atan(x);
+            // y = 0.6 * 3^x - 2.3 * x - 3
+            return 0.6 * Math.Pow(3, x) - 2.3 * x - 3;
         }
 
         // 2
-        private static void Simpson()
+        private static double Iteration()
         {
-            // #15
-            // [0; 1]
-            // n = 240
-            // f(x) = 2^(3*x)
-            // F(x) = (2^(3*x) - 1) / 3ln2
-            Console.WriteLine("\nМетод Симпсона:");
-            Console.WriteLine("f(x) = 2^(3*x)");
+            // #13
+            // [1.2; 2] -> 1.3077 
+            // y = x - 2 + Math.Sin(1 / x)
+            Console.WriteLine("\nМетод итераций:");
+            Console.WriteLine("f(x) = x - 2 + sin(1/x)");
 
-            const double a = 0, b = 1, n = 240;
-            double h = (b - a) / (2 * n);
-            // I ~= (h/3)*(y0 + y2n + sum((3+ci)*yi)) -> i from 1 to 2n-1
-            // ci = (-1)^(i+1)
-            double y0 = Function_s(a), y2n = Function_s(b), sum = 0;
-            double xi = a;
-            for (int i = 1; i < 2 * n; i++)
+            double x1 = 1.2, x2 = 2;
+
+            // Проверка на сходимость:
+            // d(phi)/dx = -cos(1/x)*(-1/x^2) = cos(1/x)/x^2
+            // Достаточное условие сходимости: |d(phi)/dx| < 1
+            if (Math.Abs(Math.Cos(1 / x2) / Math.Pow(x2, 2)) >= 1)
             {
-                xi = xi + h;
-                sum = sum + (3 + Math.Pow(-1, i + 1)) * Function_s(xi);
+                Console.WriteLine("Не удовлетворяет условию сходимости");
+                return 0;
             }
-            // По формуле Ньютона-Лейбница
-            double integral = Math.Round(Bigfunction_s(b) - Bigfunction_s(a), 4);
-            double s = Math.Round((h / 3) * (y0 + y2n + sum), 4);
-            double abs = Math.Round(s - integral, 4);
-            double otn = Math.Round(Math.Abs(abs) / integral, 4) * 100;
-            Console.WriteLine($"F(b) - F(a) = {integral}"); // действительное значение
-            Console.WriteLine($"I ~= {s}"); // эксперементальное значение
-            Console.WriteLine($"Абсолютная погрешность ~= {abs}");
-            Console.WriteLine($"Относительная точность приближенного значения интеграла ~= {otn}%");
+            else
+            {
+                int iter_counter = 0;
+                do
+                {
+                    x2 = Phi(x1);
+                    x1 = x2;
+                    iter_counter++;
+                }
+                while (Math.Abs(x1 - Phi(x1)) > 1e-5);
+                Console.WriteLine($"Количество итераций: {iter_counter}");
+                return Math.Round(x2, 4);
+            }
         }
-        // функция
-        private static double Function_s(double x)
+        private static double Phi(double x)
         {
-            return Math.Pow(2, 3 * x);
+            // y = x - 2 + Math.Sin(1 / x) = 0 => phi(x) = 2 - Math.Sin(1 / x)
+            return 2 - Math.Sin(1 / x);
         }
-        // первообразная
-        private static double Bigfunction_s(double x)
+
+        // 3
+        private static double Newton()
         {
-            return (Math.Pow(2, 3 * x) - 1) / (3 * Math.Log(2));
+            // # 14
+            // [3; 4] -> 3.5265
+            // y = e^x + lnx - 10x
+            Console.WriteLine("\nМетод Ньютона:");
+            Console.WriteLine("f(x) = e^x + lnx - 10x");
+
+            double x0 = 4, x1 = 3;
+            if (Func_n(x0) * D2Func_n(x0) > 0)
+            {
+                int ev_counter = 0;
+                do 
+                {
+                    // Зависимость x1 от x0 выводим из уравнения касательной
+                    // f(x0)/(x0-x1) = f'(x0) => x1 = x0 - f(x0)/f'(x0)
+                    x0 = x1;
+                    x1 = x0 - Func_n(x0) / DFunc_n(x0);
+                    ev_counter++;
+                }
+                while (Math.Abs(x1 - x0) > Math.Sqrt(1e-4)/10);
+                Console.WriteLine($"Количество вычеслений: {ev_counter}");
+                return Math.Round(x1, 4);
+            }
+            else
+            {
+                Console.WriteLine("Не удовлетворяет условию сходимости");
+                return 0;
+            }
+        }
+        private static double Func_n(double x)
+        {
+            return Math.Exp(x) + Math.Log(x) - 10 * x;
+        }
+        private static double DFunc_n(double x)
+        {
+            // y' = e^x + 1/x - 10
+            return Math.Exp(x) + 1 / x - 10;
+        }
+        private static double D2Func_n(double x)
+        {
+            // y" = e^x - 1/x^2
+            return Math.Exp(x) - 1 / (x * x);
         }
     }
 }
